6aab3208d5d96e467d07ce2cab481529bb213202
Tammo Laube <tammolaube@gmail.com>
2015-10-24 16:29:55 +0200

    add basic security

---
+++ src/main/groovy/com/tammolaube/site/WebSecurityConfig.groovy
@@ -0,0 +1,35 @@
+package com.tammolaube.site
+
+import org.springframework.security.web.csrf.CsrfFilter
+
+import org.springframework.http.HttpMethod
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity
+import org.springframework.security.web.csrf.CsrfTokenRepository
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository
+
+import com.tammolaube.site.filters.CsrfHeaderFilter
+
+@Configuration
+@EnableWebMvcSecurity
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/**").permitAll()
+                .anyRequest().authenticated().and()
+            .csrf().csrfTokenRepository(csrfTokenRepository()).and()
+            .addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class);
+    }
+
+    private CsrfTokenRepository csrfTokenRepository() {
+        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+        repository.setHeaderName("X-XSRF-TOKEN");
+        return repository;
+    }
+
+}
