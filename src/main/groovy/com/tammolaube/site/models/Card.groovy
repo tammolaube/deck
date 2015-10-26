3bb3916753278b662f5de8063c734423b88f9447
Tammo Laube <tammolaube@gmail.com>
2015-10-26 12:07:51 +0100

    add @lob to body of card to enable long text

--- src/main/groovy/com/tammolaube/site/models/Card.groovy
+++ src/main/groovy/com/tammolaube/site/models/Card.groovy
@@ -6,6 +6,7 @@ import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.Lob
 
 @Entity
 @EqualsAndHashCode(includes=['id'])
@@ -17,6 +18,7 @@ class Card {
     boolean side
     int priority
     String header
+    @Lob
     String body
     String footer
     String contextClass
